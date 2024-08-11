d466667e0df0e5c93214b2799d3dcb9e671ea542
syed <myselfmubeen123@gmail.com>
2024-08-11 11:43:55 +0530

    solar_sys

---
+++ solar_sys/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+           <div class="sun"></div>
+           <div class="earth">
+               <div class="moon"></div>
+           </div>
+       </div>
+  );
+}
+
+export default App;
